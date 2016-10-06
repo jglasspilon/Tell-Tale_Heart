d37b8cf3017b4264e884ade58c6b7cf47c031f3e
Julian Glass-Pilon <jglasspilon@live.com>
2016-10-05 20:52:43 -0400

    Added scale by unit script for prefab objects

---
+++ Assets/Dungeon/ScaleByUnit.cs
@@ -0,0 +1,15 @@
+﻿// Summary:
+//
+// Created by: Julian Glass-Pilon
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class ScaleByUnit : MonoBehaviour 
+{
+	void Start()
+    {
+        transform.localScale = new Vector3(DungeonCreator.unitSize, transform.localScale.y, DungeonCreator.unitSize);
+    }
+}
